8e90a2956cf4ca8b2fbd9349f437ce0bc95bda99
Kristen Rieger <karieger1@gmail.com>
2015-06-29 22:51:12 -0500

    why you no work

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,1 +1,47 @@
-$(document).on('ready', start);
+$(document).on("ready", function() {
+
+
+
+$("#button").on("click", login);
+
+var email = $("#email").val();
+var password = $("#password").val();
+
+	function login(e) {
+		e.preventDefault();
+
+	if (email ==="aaron@theironyard.com" && password === "password123") {
+		console.log("success?");
+		window.location.replace = ("http://www.theironyard.com");
+	}
+
+	if (email ==="admin@google.com" && password === "pandas") {
+		window.location.href = ("http://www.theironyard.com");
+	}
+
+	if (email =="butts@butts.com" && password === "honeycrisp") {
+		window.location.href = ("http://www.theironyard.com");
+	}
+
+	if (email.indexOf("@")=== -1) {
+		$("#email-error").html("enter an email address!");
+	}
+
+	if(password === "") {
+		$("#password-error").html("enter a password!");
+	}
+
+	if (email !== "aaron@theironyard.com" || email !== "admin@google.com" || email !== "butts@butts.com") {
+		$("#email-error").html("invalid username");
+	}
+
+	if(email === "aaron@theironyard.com" && password !== "password123"){
+			$("#password-error").html("email does not match password.");
+		}
+	
+	if(email === "admin@google.com" && password !== "pandas"){
+			$("#password-error").html("email does not match password.");
+		}
+
+	}
+});
